3d2f21151f0a9b19bada62521511fb4c4cf056c4
Kevin Palmer <kpalmer4194@gmail.com>
2019-07-31 22:33:18 -0400

    working yet?

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,27 +1,27 @@
-import React from "react";
+// import React from "react";
 
-const Jumbotron_color =  {
-  height: 100%,
-  clear: "both",
-  paddingTop: 120,
-  textAlign: "center",
-  backgroundColor :  '#000'
-}
+// const Jumbotron_color =  {
+//   height: 100%,
+//   clear: "both",
+//   paddingTop: 120,
+//   textAlign: "center",
+//   backgroundColor :  '#000'
+// }
 
 // {[{}]
 //   backgroundcolor :  '#000'
 
-//   }
+// //   }
 
-function Jumbotron({ children }) {
-  return (
-    <div
-      style={Jumbotron_color}
-      className="jumbotron"
-    >
-      {children}
-    </div>
-  );
-}
+// function Jumbotron({ children }) {
+//   return (
+//     <div
+//       style={Jumbotron_color}
+//       className="jumbotron"
+//     >
+//       {children}
+//     </div>
+//   );
+// }
 
-export default Jumbotron;
+// export default Jumbotron;
